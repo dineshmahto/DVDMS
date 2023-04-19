66ae03b9c6fc35443c28432324a73e2d591df686
Dinesh <dineshkumarmahto93@gmail.com>
2023-04-18 22:09:29 -0700

    18-04-2023 changes

--- src/common/constant/constants.js
+++ src/common/constant/constants.js
@@ -6,10 +6,22 @@ export const LOGOUT = "auth/logout";
 export const GET_NOTIFICATION_LIST = "pagination/notificationList";
 // Stock
 export const STOCK_LISTING = "pagination/calls/stocklisting";
-// Admin
+// Admin Module
 // Role Desk
 export const ROLE_LISTING = "pagination/roleList";
-export const ALL_ACTIVITY_TYPE_LIST = "admin/getAllActivityTypeList";
-export const ACTIVITY_LIST_BY_CODE = "admin/getActivityListByType";
 //User Desk
 export const USER_LISTING = "pagination/userList";
+// Progame Desk
+export const PROGRAME_DESK_LISTING = "pagination/programmeList";
+// Store Desk
+
+// End of Admin Module Route
+
+// Order Management Module
+// Purchase Order List Route
+export const PURCHASE_ORDER_LIST = "pagination/purchaseOrderList";
+export const SUPPLIER_LIST = "pagination/supplierList";
+
+export const STORE_DESK_LISTING = "pagination/storeList";
+export const ALL_ACTIVITY_TYPE_LIST = "admin/getAllActivityTypeList";
+export const ACTIVITY_LIST_BY_CODE = "admin/getActivityListByType";
